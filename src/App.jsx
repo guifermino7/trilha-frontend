4240a5b23aa14ebc342ecc86c630ec490c5ba74b
Guilherme <guigamf@hotmail.com>
2024-06-22 02:22:19 -0300

    Alterando site de cursos para site pessoal adicionando biografia

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './styles/App.css';
 import { Navbar } from "./components/Navbar/Navbar";
 import { Article } from "./components/Article/Article";
 import { Counter } from "./components/Counter/Counter";
+import { Aboutme } from "./components/Aboutme/Aboutme";
 
 import article1Img from "./assets/images/html-logo.png";
 import article2Img from "./assets/images/css-logo.png";
@@ -16,6 +17,10 @@ class App extends React.Component {
       <>
         <Navbar/>
   
+        <section id="bio">
+              <Aboutme/>
+        </section>
+
         {/* <Counter/> */}
 
         <section id="articles">
@@ -23,25 +28,29 @@ class App extends React.Component {
           title="Curso de HTML"
           provider="Ada Tech"
           description="Este é um curso de HTML, onde é fundamental consolidar conhecimentos para poder aprender React."
-          thumbnail={article1Img}/>
+          thumbnail={article1Img}
+          url="https://comunidade.ada.tech/cursos/829a58a4-5871-410f-883a-4653fcccb0b6"/>
 
           <Article
           title="Curso de CSS"
           provider="Ada Tech"
           description="Este é um curso de CSS, onde é fundamental consolidar conhecimentos para poder aprender React."
-          thumbnail={article2Img}/>
+          thumbnail={article2Img}
+          url="https://comunidade.ada.tech/cursos/7b554e4d-867e-4859-a1ff-5effae0d5ff9"/>
 
           <Article
           title="Curso de Javascript"
           provider="Ada Tech"
           description="Este é um curso de Javascript, onde é fundamental consolidar conhecimentos para poder aprender React."
-          thumbnail={article3Img}/>
+          thumbnail={article3Img}
+          url="https://comunidade.ada.tech/cursos/1eee6337-f72e-61e0-f36a-9477308320a9"/>
 
           <Article
           title="Curso de React"
           provider="Ada Tech"
           description="Este é um curso de React, onde irá unir tudo que foi estudando nos cursos de HTML, CSS e Javascript junto com funcionalidades do React."
-          thumbnail={article4Img}/>
+          thumbnail={article4Img}
+          url="https://comunidade.ada.tech/cursos/b60dfd96-34d9-4880-b23e-7e3679eb5391"/>
         </section>
       </>
     );
