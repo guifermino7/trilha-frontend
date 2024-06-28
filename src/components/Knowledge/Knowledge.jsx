f766961e09d047bd784cbb5ec5ee6b77b366846a
Guilherme <guigamf@hotmail.com>
2024-06-27 23:33:52 -0300

    Adicionando o Knowledge, onde terá as informações de cursos realizados por mim

---
+++ src/components/Knowledge/Knowledge.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./styles.css";
+
+export class Knowledge extends React.Component {
+    render() {
+        return (
+                <article id="knowledge">
+                    <a href={this.props.url} target="_blank">
+                           <img
+                            src={this.props.thumbnail}
+                            alt={this.props.title}
+                            className="logo"/>
+                    </a>
+                    <div className="knowledge-infos">
+                        <h2>{this.props.title}</h2>
+                        <h3>{this.props.provider}</h3>
+
+                        <p>{this.props.description}</p>
+                    </div>
+                </article >
+        )
+    }
+}
