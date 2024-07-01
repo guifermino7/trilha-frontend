ece5283a6b7520065d619fc2375b0192f6517be7
Guilherme <guigamf@hotmail.com>
2024-07-01 00:47:21 -0300

    Modificando o Knowledge para ficar informações de cursos que fiz junto com meus projetos

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import article1Img from "./assets/images/html-logo.png";
 import article2Img from "./assets/images/css-logo.png";
 import article3Img from "./assets/images/javascript-logo.png";
 import article4Img from "./assets/images/react-logo.png";
+import marioGame from "./assets/images/mario-icon.png";
 
 class App extends React.Component {
   constructor() {
@@ -33,29 +34,27 @@ class App extends React.Component {
 
                     <ul className="nav-list">
                         <li><a href="/">Home</a></li>
-                        <li><a className="knowledge-button"
-                                onClick={() => {
+                        <li><button onClick={() => {
                                     this.setState({ showKnowledge: !this.state.showKnowledge })
-                                }}>Meu conhecimento</a></li>
+                                }}>Meu conhecimento</button></li>
                         <li><a href="https://www.linkedin.com/in/guilhermefermino/" target="_blank" rel="noreferrer">LinkedIn</a></li>
                         <li><a href="https://github.com/guifermino7" target="_blank" rel="noreferrer">GitHub</a></li>
                     </ul>
                 </nav>
         </header>
   
-        <section className="intro">
-            { this.state.showKnowledge ? null : <Aboutme/> 
-              }
+        <section className="intro" style={{display: this.state.showKnowledge ? "none" : "flex"}}>
+            { this.state.showKnowledge ? null : <Aboutme/> }
         </section>
 
-        <section className="bio">
+        <section className="bio" style={{display: this.state.showKnowledge ? "none" : "flex"}}>
             { this.state.showKnowledge ? null : <Experience/> }
             { this.state.showKnowledge ? null : <Skills/> }
         </section>
 
         {/* <Counter/> */}
 
-        <section id="myknowledge">
+        <section id="myknowledge" style={{display: this.state.showKnowledge ? "flex" : "none"}}>
           { this.state.showKnowledge ? <Knowledge
           title="Curso de HTML"
           provider="Ada Tech"
--- src/components/Knowledge/Knowledge.jsx
+++ src/components/Knowledge/Knowledge.jsx
@@ -1,23 +1,43 @@
 import React from "react";
 import "./styles.css";
 
+import marioGame from "../../assets/images/mario-icon.png";
+
 export class Knowledge extends React.Component {
     render() {
         return (
+            <div className="knowledge-container">
                 <article id="knowledge">
-                    <a href={this.props.url} target="_blank">
+                    <a href={this.props.url} target="_blank" rel="noreferrer">
                            <img
                             src={this.props.thumbnail}
                             alt={this.props.title}
                             className="logo"/>
                     </a>
-                    <div className="knowledge-infos">
+
+                    <div className="knowledge-info">
                         <h2>{this.props.title}</h2>
                         <h3>{this.props.provider}</h3>
 
                         <p>{this.props.description}</p>
                     </div>
                 </article >
+
+                <article id="project">
+                    <a href="https://guifermino7.github.io/jogo-mario/" target="_blank" rel="noreferrer">
+                        <img
+                            src={marioGame}
+                            alt={this.props.title}
+                            className="icone" />
+                    </a>
+
+                    <div className="project-info">
+                        <h2>Projeto Mario Jump</h2>
+
+                        <p>Este é um projeto utilizando apenas HTML, CSS e Javascript a fim de treinar o conhecimento adquirido nos cursos. O projeto é um jogo onde tem como objetivo fazer com que o Mario pule os Koopas (tartarugas) sem tocar nelas, por que ao tocar ele morre e perde o jogo.</p>
+                    </div>
+                </article>
+            </div>
         )
     }
 }
